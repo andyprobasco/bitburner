40de05a42c1d08cdca0ad0bc9ae2e1af289d6d29
Andy <aprobasco@gmail.com>
2020-12-30 17:24:23 -0500

    bugfix

--- rsync.ns.js
+++ rsync.ns.js
@@ -1,3 +1,5 @@
+// wget https://raw.githubusercontent.com/andyprobasco/bitburner/master/rsync.ns.js /rsync.ns
+
 const GIT_ROOT = 'https://raw.githubusercontent.com/andyprobasco/bitburner'
 const GIT_BRANCH = 'master'
 const BITBURNER_ROOT = '/rsync';
@@ -7,12 +9,10 @@ const FILES = [
     'scripts/grow.script',
 ]
 
-
-
 export async function main(ns) {
     const branchArg = ns.args.find(arg => arg.startsWith('-b=') || arg.startsWith('--branch='));
     const branch = branchArg ? branchArg.split('=')[1] : GIT_BRANCH;
     FILES.forEach(path =>{
-        ns.wget(`${GIT_ROOT}/${branch}/${filePath}.js`, `${BITBURNER_ROOT}/${path}`);
+        ns.wget(`${GIT_ROOT}/${branch}/${path}.js`, `${BITBURNER_ROOT}/${path}`);
     });
 }
